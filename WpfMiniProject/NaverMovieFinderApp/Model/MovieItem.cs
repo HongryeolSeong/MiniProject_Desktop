980c32d9c06ce47307275307b1caa70ca072c127
ghd0276 <ghd0276@naver.com>
2021-04-01 17:18:24 +0900

    4/1 Open Api 활용 MovieFinder 작업 중

---
+++ WpfMiniProject/NaverMovieFinderApp/Model/MovieItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaverMovieFinderApp.Model
+{
+    class MovieItem
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Image { get; set; }
+        public string SubTitle { get; set; }
+        public string PubDate { get; set; }
+        public string Director { get; set; }
+        public string Actor { get; set; }
+        public string UserRating { get; set; }
+
+        public MovieItem(string title, string link, string image, string subTitle,
+                         string pubDate, string director, string actor, string userRating)
+        {
+            Title = title;
+            Link = link;
+            Image = image;
+            SubTitle = subTitle;
+            PubDate = pubDate;
+            Director = director;
+            Actor = actor;
+            UserRating = userRating;
+        }
+    }
+}
